7b5f613c39588eba6a2891ea54d289ffbc8afd78
viitorfernandess <vitorazevedofer007@gmail.com>
2025-09-18 10:47:38 -0300

    alterações na  aplicação

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const button = document.querySelector(".button-add-task")
 const input = document.querySelector(".input-task")
+const completeList = document.querySelector(".list-tasks")
 
 let listItems = []
 
 function addNewTask() {
     listItems.push(input.value)
 
-    showTask()
+    input.value = ''
+
+    showTasks()
 }
 
-function showTask() {
+function showTasks() {
 
+    let newLi = ''
+
+    listItems.forEach((task, index) => {
+
+        newLi = newLi +
+            `
+            <li class="task">
+                <img src="./img/checked.png" alt="check-task">
+                <p>${task}</p>
+                <img src="./img/trash.png" alt="delete-task" onclick="deleteItem(${index})">
+            </li>
+        `
+    })
+
+    completeList.innerHTML = newLi
+
+}
+
+function deleteItem(index) {
+    listItems.splice(index, 1)
+
+    showTasks()
 }
 
 button.addEventListener('click', addNewTask)
